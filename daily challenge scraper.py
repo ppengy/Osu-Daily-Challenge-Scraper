9d9f0fee155aa58f3eb29d9a57cceff87b7f1f0b
ppengy <31017611+ppengy@users.noreply.github.com>
2024-10-14 04:19:57 -0400

    first upload

---
+++ daily challenge scraper.py
@@ -0,0 +1,58 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from datetime import datetime
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+import time
+
+
+def startDriver():
+    profile = FirefoxProfile()
+    profile.set_preference("browser.cache.disk.enable", True)
+    profile.set_preference("browser.cache.memory.enable", True)
+    profile.set_preference("browser.cache.offline.enable", True)
+    options = FirefoxOptions()
+    # options.add_argument("--headless")
+    options.set_preference("permissions.default.image", 2)
+    driver = webdriver.Firefox(options=options)
+    return driver
+
+
+def getlinksToDate(driver, output):
+    date = datetime.now().strftime("%Y-%m-%d")
+    driver.get("https://osu.ppy.sh/rankings/daily-challenge/" + date)
+    DailyChalLink = driver.find_elements(
+        By.XPATH, "//html/body/div[8]/div[2]/div[1]/div/div[2]/a"
+    )
+    for link in DailyChalLink:
+        line = link.get_attribute("href")
+        output.write(f"{line}\n")
+    return DailyChalLink
+
+
+def scrape_DailyChal(driver, link):
+    driver.get(link)
+    DailyChalLink = driver.find_element(
+        By.XPATH, "/html/body/div[8]/div[3]/div[1]/div/div/a"
+    ).get_attribute("href")
+    print("Beatmap Challenge Link: " + DailyChalLink)
+    return DailyChalLink
+
+
+driver = startDriver()
+outputFile = open(r"C:\IHATEONEDRIVE\osuDailyBot\beatmap links.txt", "w")
+inputFile = open(r"temp.txt", "w")
+getlinksToDate(driver, inputFile)
+inputFile = open(r"temp.txt", "r")
+L = inputFile.readlines()
+for line in L:
+    DCLink = line.strip()
+    DCDate = DCLink.split("/")[-1]
+    print("Current Daily Challenge: " + DCDate)
+    time.sleep(2.5)
+    beatmapLink = scrape_DailyChal(driver, DCLink)
+    data = f"{DCDate} {beatmapLink}\n"
+    outputFile.write(data)
+outputFile.close()
+inputFile.close()
+driver.quit()
